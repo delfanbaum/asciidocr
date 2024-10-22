aa8e2fcaf19c7ead294da3589fb9976b9441bf96
delfanbaum <delfanbaum@oreilly.com>
2024-10-22 11:58:28 -0400

    Reorganize tests and add attr tests

--- tests/block_tests.rs
+++ tests/block_tests.rs
@@ -1,27 +1,11 @@
-use std::fs;
-
-use asciidocr::{parser::Parser, scanner::Scanner};
-use assert_json_diff::assert_json_eq;
+mod common;
+use common::assert_parsed_doc_matches_expected_asg;
 use rstest::rstest;
-use serde_json::{json, Value};
-
-fn assert_parsed_doc_matches_expected_asg(adoc_fn: &str, asg_json_fn: &str) {
-    let parsed_asg = json!(Parser::new().parse(Scanner::new(
-        &fs::read_to_string(adoc_fn).expect(&format!("Unable to open {}", adoc_fn))
-    )));
-
-    let expected_asg: Value = serde_json::from_str(
-        &fs::read_to_string(asg_json_fn).expect(&format!("Unable to open {}", asg_json_fn)),
-    )
-    .unwrap();
-
-    assert_json_eq!(parsed_asg, expected_asg);
-}
 
 #[rstest]
-#[case("tests/blocks/para-internal-line-break")]
-#[case("tests/blocks/para-single-line")]
-#[case("tests/blocks/para-two-paras-line-between")]
+#[case::para_single_line("blocks/para-single-line")]
+#[case::para_two_lines_space_between("blocks/para-two-paras-line-between")]
+#[case::para_internal_line_break("blocks/para-internal-line-break")]
 fn test_paragraphs(#[case] fn_pattern: &str) {
     let adoc_fn = format!("{}.adoc", fn_pattern);
     let asg_json_fn = format!("{}.json", fn_pattern);
@@ -29,7 +13,8 @@ fn test_paragraphs(#[case] fn_pattern: &str) {
 }
 
 #[rstest]
-#[case("tests/blocks/document-header")]
+#[case::header_title_only("blocks/document-header")]
+#[case::header_attrs("blocks/document-attr")]
 fn test_document(#[case] fn_pattern: &str) {
     let adoc_fn = format!("{}.adoc", fn_pattern);
     let asg_json_fn = format!("{}.json", fn_pattern);
