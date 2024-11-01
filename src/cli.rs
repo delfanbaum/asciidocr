f41e3457a72306a30cfc1fc574424689f1f6d2e5
delfanbaum <delfanbaum@oreilly.com>
2024-11-01 16:29:12 -0400

    CLI improvements

--- src/cli.rs
+++ src/cli.rs
@@ -5,12 +5,16 @@ use clap::Parser;
 #[command(name = "asciidocr")]
 #[command(about="A Rust CLI and library for processing Asciidoc files.", long_about = None)]
 pub struct Cli {
-    /// Asciidoc file for processing. To provide asciidoc from standard input (stdin), use "-"
+    /// Asciidoc file for processing. To read from standard input (stdin), use "-".
     pub file: String,
+
+    /// Optionally, provide a filename for the output. Defaults to FILE.<format>.
+    /// To send to standard out (stdout), use "-".
+    #[arg(short = 'o', long = "output-file")]
+    pub output: Option<String>,
 
     /// Flag for returning the asciidoc Abstract Syntax Graph (asg); used for validation
     /// with the official Asciidoc Technology Compatibility Kit (TCK).
     #[arg(short = 'a', long = "tck-adapter")]
     pub adapter: bool,
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,48 @@
+use anyhow::Result;
 use clap::Parser;
-use std::{fs, io};
+use std::{fs, io, path::PathBuf};
 
 use asciidocr::{cli::Cli, output::render, parser::Parser as AdocParser, scanner::Scanner};
 
 fn main() {
     let args = Cli::parse();
+    let destination = match args.output {
+        Some(ref output) => {
+            if output == "-" {
+                None
+            } else {
+                let mut out_destination = PathBuf::from(args.file.clone());
+                out_destination.set_extension("html");
+                Some(out_destination)
+            }
+        }
+        None => {
+            let mut out_destination = PathBuf::new();
+            out_destination.push(args.file.clone());
+            if args.adapter {
+                out_destination.set_extension("json")
+            } else {
+                out_destination.set_extension("html")
+            };
+            Some(out_destination)
+        }
+    };
+
+    match run(args) {
+        Ok(result) => output(result, destination),
+        Err(e) => {
+            eprintln!("Error converting document: {}", e);
+            std::process::exit(1)
+        }
+    }
+}
+
+fn run(args: Cli) -> Result<String> {
     let graph = AdocParser::new().parse(Scanner::new(&open(&args.file)));
     if args.adapter {
-        println!(
-            "{}",
-            serde_json::to_string_pretty(&graph).expect("Failed to parse graph")
-        );
+        Ok(serde_json::to_string_pretty(&graph)?)
     } else {
-        match render(&graph) {
-            Ok(rendered) => println!("{}", rendered),
-            Err(e) => eprintln!("Error: {}", e),
-        }
+        render(&graph)
     }
 }
 
@@ -24,5 +51,12 @@ fn open(filename: &str) -> String {
         "-" => io::read_to_string(io::stdin()).expect("Error reading from stdin"),
         _ => fs::read_to_string(filename)
             .unwrap_or_else(|_| panic!("Unable to read file: {}", filename)),
+    }
+}
+
+fn output(result: String, destination: Option<PathBuf>) {
+    match destination {
+        Some(out_file) => fs::write(out_file, result).expect("Error writng file"),
+        None => println!("{}", result),
     }
 }
