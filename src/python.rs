1f81670619259aade5786b271e3354621dadbfbf
Danny Elfanbaum <drelfanbaum@gmail.com>
2025-01-12 07:48:25 -0500

    add json out for easier in-app use

--- src/python.rs
+++ src/python.rs
@@ -1,18 +1,33 @@
-#![cfg(feature="python")]
+#![cfg(feature = "python")]
 
-use std::path::PathBuf;
-use crate::scanner;
-use crate::parser;
 use crate::backends::htmls::render_htmlbook;
+use crate::parser;
+use crate::scanner;
 use pyo3::{exceptions::PyRuntimeError, prelude::*};
+use std::path::PathBuf;
 
-/// parses a string using the specified backend
+/// parses an asciidoc string to HTML (using the default HTML build)
 #[pyfunction]
 fn parse_to_html(adoc_str: &str) -> PyResult<String> {
     let graph = parser::Parser::new(PathBuf::from("-")).parse(scanner::Scanner::new(adoc_str));
     match render_htmlbook(&graph) {
         Ok(html) => Ok(html),
-        Err(_) => Err(PyRuntimeError::new_err("Error converting asciidoc string")),
+        Err(_) => Err(PyRuntimeError::new_err(
+            "Error converting asciidoc string to HTML",
+        )),
+    }
+}
+
+/// parses an asciidoc string to a Json string, which can then be converted using Python's
+/// json package from standard library (or some other function(s)) into a dict
+#[pyfunction]
+fn parse_to_json_str(adoc_str: &str) -> PyResult<String> {
+    let graph = parser::Parser::new(PathBuf::from("-")).parse(scanner::Scanner::new(adoc_str));
+    match serde_json::to_string_pretty(&graph) {
+        Ok(json_str) => Ok(json_str),
+        Err(_) => Err(PyRuntimeError::new_err(
+            "Error converting asciidoc string to JSON",
+        )),
     }
 }
 
