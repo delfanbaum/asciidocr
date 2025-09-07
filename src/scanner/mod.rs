0470b44b2b89110b5e91b69ee52b9bd32ddb4efa
Danny Elfanbaum <drelfanbaum@gmail.com>
2025-09-07 13:34:54 -0500

    Make parser error for invalid heading level(s) matchable

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -11,8 +11,8 @@ use self::tokens::TokenError;
 pub enum ScannerError {
     #[error(transparent)]
     Token(#[from] TokenError),
-    #[error("General error: `{0}`")]
-    String(String),
+    #[error("Invalid headling level at line {0}")]
+    HeadingLevelError(usize),
     #[error("Invalid include tag pattern: {0}")]
     TagError(String),
 }
@@ -505,9 +505,7 @@ impl<'a> Scanner<'a> {
             3 => self.add_token(TokenType::Heading3, false, 0),
             4 => self.add_token(TokenType::Heading4, false, 0),
             5 => self.add_token(TokenType::Heading5, false, 0),
-            _ => {
-                return Err(ScannerError::String(format!("Invalid headling level: line {}", self.line)))
-            }
+            _ => Err(ScannerError::HeadingLevelError(self.line)),
         }
     }
 
