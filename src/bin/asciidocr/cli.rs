6bf25bb1bee4941cf6987d3c25c28c79b670448e
Danny Elfanbaum <drelfanbaum@gmail.com>
2025-09-16 19:58:03 -0400

    Verbosity flag
    Doesn't do a whole lot yet but that's OK

--- src/bin/asciidocr/cli.rs
+++ src/bin/asciidocr/cli.rs
@@ -28,6 +28,10 @@ pub struct Cli {
     #[arg(short, long = "embedded")]
     pub embedded: bool,
 
+    /// Show verbose processing output
+    #[arg(short, long)]
+    pub verbose: bool,
+
     /// Run asciidocr without resolving resource targets; by default, asciidocr will fail to parse a
     /// document if a given resource is not found. This option allows parsing to continue if the
     /// backend supports missing targets.
